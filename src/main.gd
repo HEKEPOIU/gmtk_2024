e688d14b89ac34fe9d90dae2da86e7257d0c20db
green-Desktop <appieosugood@gmail.com>
2024-08-18 21:43:39 +0800

    fix typo

--- src/main.gd
+++ src/main.gd
@@ -2,13 +2,13 @@ extends Node
 
 class_name test
 
-@onready var canvas := get_node("SubViewport/Canvas") as UiMain
+@onready var canvas := get_node("CanvasLayer/Canvas") as UiMain
 @onready var camera2d := get_node("Camera2D") as Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	canvas.game_start.connect(onStart)
-	pass  # Replace with function body.
+	pass # Replace with function body.
 
 
 func onStart() -> void:
@@ -17,5 +17,5 @@ func onStart() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-
+	
 	pass
