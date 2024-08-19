6a6b63fb8649de9a9dadec4b5d8ead7c7e91d772
Hekepoiu <decnovpk92805@gmail.com>
2024-08-19 22:33:43 +0800

    Bind camera system, and base pendulum_manager

--- scene/camera_manager.gd
+++ scene/camera_manager.gd
@@ -1,19 +1,24 @@
 extends Camera2D
 
-@onready var camera2d := get_node("Camera2D") as Camera2D
+class_name CameraManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var move_duration: float = 0.2
+var follow_pos: Vector2
+var zoom_size: Vector2
 
-func _tween_to_fit_screen(scale: Vector2, position: Vector2) -> void:
 
+func tween_to_fit_screen(target_position: Vector2, target_scale: Vector2) -> void:
 	var tween := create_tween()
-	tween.tween_property(camera2d, "zoom", scale, 1)
+	tween.tween_property(self, "zoom", Vector2.ONE / target_scale, move_duration)
 	var tween1 := create_tween()
-	tween1.tween_property(camera2d, "position", position, 1)
+	tween1.tween_property(self, "position", target_position, move_duration)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	tween_to_fit_screen(follow_pos, zoom_size)
+
+
+func deal_follow(new_follow: Pendulum) -> void:
+	follow_pos = new_follow.global_position + Vector2(0, new_follow.length)
+	print(new_follow.length)
+	zoom_size = new_follow.length / 800 * Vector2.ONE
--- src/pendulum.gd
+++ src/pendulum.gd
@@ -4,12 +4,13 @@ class_name Pendulum
 # follow this video:
 # https://www.youtube.com/watch?v=J1ClXGZIh00
 signal start
+signal be_current(pendulum: Pendulum)
 var pivot_pos: Vector2
 var end_position: Vector2
 var angle: float
-var move_dir: int = 0
 var prev_pos: Vector2
 var is_simulating: bool = true
+var is_moving: bool = false
 @export var move_velocity: float = 5
 @export var is_follow: bool = true
 
@@ -26,6 +27,7 @@ var damping: float:
 @export var max_mass: float = 1000
 @export var is_current: bool = false
 @export var end_point_image: Texture2D
+@export var max_angular_velocity: float = 0.5
 
 var angular_velocity: float = 0.0
 var angular_acceleration: float = 0.0
@@ -39,6 +41,7 @@ func _ready() -> void:
 	end_point.on_drag_stop.connect(start_simulate)
 	end_point.set_scale_base_mass(mass, min_mass, max_mass)
 	end_point.set_sprite(end_point_image)
+	prev_pos = global_position
 	set_mass(mass)
 	set_start_position(rotation)
 
@@ -48,22 +51,9 @@ func _physics_process(delta: float) -> void:
 		return
 	var pos_diff := global_position - prev_pos
 	prev_pos = global_position
-	process_velocity(delta, pos_diff)
 	update_position(delta, pos_diff)
-	move(delta)
 	set_end_point_position(length)
-
-
-# func _process(_delta: float) -> void:
-# 	if Input.is_key_pressed(KEY_LEFT):
-# 		move_dir -= 1
-# 	if Input.is_key_pressed(KEY_RIGHT):
-# 		move_dir += 1
-
-
-func move(delta: float) -> void:
-	position.x += move_dir * move_velocity * delta
-	move_dir = 0
+	process_velocity(delta, pos_diff)
 
 
 func set_start_position(init_angle: float) -> void:
@@ -85,13 +75,15 @@ func set_end_point_position(arm_length: float) -> void:
 
 func update_position(delta: float, pos_dif: Vector2) -> void:
 	if is_follow:
-		angle += move_dir * delta * abs(pos_dif.x) * delta * end_point_move_multiplier
+		angular_velocity += delta * clamp(pos_dif.x, -1, 1) * delta * end_point_move_multiplier
 
 
 func process_velocity(delta: float, _pivot_pos_diff: Vector2) -> void:
 	angular_acceleration = (-gravity * delta) / length * sin(angle)
 	angular_velocity += angular_acceleration
 	angular_velocity *= damping
+
+	angular_velocity = clamp(angular_velocity, -max_angular_velocity * delta, max_angular_velocity * delta)
 	angle += angular_velocity
 
 	rotation = angle
@@ -109,29 +101,32 @@ func set_mass(new_mass: float) -> void:
 
 
 func deal_collide(other: Pendulum) -> void:
-	if not is_current:
+	if not is_current and other.is_current:
 		is_current = true
+		be_current.emit(self)
 		return
-	var percent: float = mass / other.mass
-	# u = (m_s * v_s + m_o * v_o) / (m_s + m_o)
-	# https://zh.wikipedia.org/zh-tw/%E6%93%BA
-	var new_velocity := (
-		(mass * angular_velocity + other.mass * other.angular_velocity)
-		/ (mass + other.mass)
-		* percent
-	)
+	if not is_moving:
+		var percent: float = mass / other.mass
+		# u = (m_s * v_s + m_o * v_o) / (m_s + m_o)
+		# https://zh.wikipedia.org/zh-tw/%E6%93%BA
+		var new_velocity := (
+			(mass * angular_velocity + other.mass * other.angular_velocity)
+			/ (mass + other.mass)
+			* percent
+		)
 
-	other.set_mass(mass + other.mass)
-	other.add_velocity(new_velocity)
+		other.add_velocity(new_velocity)
 
-	queue_free()
+		other.set_mass(mass + other.mass)
+		queue_free()
 
 
 func be_drag() -> void:
 	if not is_current:
 		return
-	# TODO: this will cause line not follow when mouse to fast
+	is_simulating = true
 	var dir_to_mouse := (get_global_mouse_position() - pivot_pos).normalized()
+	print(get_global_mouse_position())
 	var mouse_angle := -dir_to_mouse.angle_to(Vector2.DOWN)
 	set_start_position(mouse_angle)
 
